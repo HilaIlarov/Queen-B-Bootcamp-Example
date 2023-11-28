71b21c57f469aad5107562fb6fd47d8d2e95f65d
yara salami <yara16salami@gmail.com>
2023-11-28 10:40:42 +0200

    Add initial pages to the 'pages' folder
    Added two starter files to the 'pages' folder to kickstart the development of additional pages within the project. These files serve as a foundation for further page implementations and enhancements.

--- client/src/pages/Mentor.jsx
+++ client/src/pages/Mentor.jsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {useLocation} from "react-router-dom";
+import DOMPurify from "dompurify";
 
 const Mentor = () => {
+    const [mentor, setMentor] = useState({});
+
+    const  location = useLocation();
+
+    const mentorId = location.pathname.split("/")[1];
+    console.log("hey?   ############");
+
+    useEffect(() => {
+        const fetchData = async ()=>{
+            try{
+                const res = await axios.get(`/mentors/${mentorId}`);
+                setMentor(res.data);
+            }catch (err){
+                console.log(err);
+            }
+        };
+        fetchData();
+    }, [mentorId]);
+
+    const getText = (html) =>{
+        const doc = new DOMParser().parseFromString(html, "text/html")
+        return doc.body.textContent
+    }
+
     return (
-        <div>
-            <h1> Mentor ... </h1>
+        <div className="Mentor">
+            <div className="content">
+                <img src={`${mentor.img}`} alt="" />
+                <h1>{getText(mentor.first_name)} {getText(mentor.last_name)}</h1>
+                <p>{getText(mentor.profession)}</p>
+                <p
+                    dangerouslySetInnerHTML={{
+                        __html: DOMPurify.sanitize(mentor.description),
+                    }}
+                ></p>
+                <div className="media">
+                    <a href={mentor.instagram} target="_blank" rel="noopener noreferrer">
+                        <img
+                            src="https://www.pngkit.com/png/full/1-13187_instagram-logo-new-vector-eps-free-download-logo.png"
+                            alt="Instagram"
+                            style={{ width: "100px", height: "100px" }} // Adjust the image size as needed
+                        />
+                    </a>
+                </div>
+
+            </div>
         </div>
     );
-};
 
+};
 export default Mentor;
--- client/src/pages/Mentors.jsx
+++ client/src/pages/Mentors.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Mentors = () => {
+    const [mentors, setMentors] = useState([])
+
+    useEffect(() => {
+        const fetchData = async ()=>{
+            try{
+                const res = await axios.get("/mentors");
+                setMentors(res.data);
+            }catch (err){
+                console.log(err);
+            }
+        };
+        fetchData();
+    }, []);
+    const getText = (html) =>{
+        const doc = new DOMParser().parseFromString(html, "text/html")
+        return doc.body.textContent
+    }
+
     return (
-        <div>
-            <h1> Mentors ... </h1>
+        <div className="Mentors">
+            <div className="members">
+                {mentors.map((mentor) => (
+                    <div className="member" key={mentor.mentor_id}>
+                        <div className="img">
+                            <Link className="link" to={`/${mentor.mentor_id}`}>
+                                <img src={`${mentor.img}`} alt="" />
+                            </Link>
+                        </div>
+                        <div className="content">
+                            <p>{getText(mentor.first_name)} {getText(mentor.last_name)}</p>
+                            <p>{getText(mentor.profession)}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
