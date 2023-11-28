71dfe34b0b62b48b78fdf6227acaa14c8c68ce69
yara salami <yara16salami@gmail.com>
2023-11-28 17:35:38 +0200

    Update navbar and footer components
    display user login status.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import Logo from "../images/QueenB.png";
 import { Link } from "react-router-dom";
 
+
 const Footer = () => {
 	return (
 		<footer>
-			<img src={Logo} alt="" />
+			<Link to="/">
+    			<img src={Logo} alt=""/>
+			</Link>
 		</footer>
 	);
 };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,32 +1,29 @@
 import React, { useContext } from "react";
 import Logo from "../images/QueenB.png";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
 const Navbar = () => {
-	return (
-		<div className="navbar">
-			<div className="container" style={styles.nav}>
-				<div className="logo">
-					<Link ClassName="link" to="/">
-						<h6>Home</h6>
-						{/* <img src={Logo} alt="" style={{ height: "50px", }}/> */}
-					</Link>
-				</div>
-				<div className="links">
-					<Link ClassName="link" to="/mentors">
-						<h6>Mentors</h6>
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
-};
+    const { currentUser, logout } = useContext(AuthContext);
 
-const styles = {
-	nav: { flexDirection: "row", 
-    display: "flex",
-    // justifyContent: "space-around",
-    alignItems: "center", },
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <Link ClassName="link" to="/">
+                        <img src={Logo} alt=""/>
+                    </Link>
+                </div>
+                <div className="links">
+                    <Link ClassName="link" to="/mentors">
+                        <h6>Mentors</h6>
+                    </Link>
+                    <span>{currentUser?.name}</span>
+                    {currentUser ? (<span onClick={logout}>Logout</span>) : (<Link className="link" to="/login">Login</Link>)}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
