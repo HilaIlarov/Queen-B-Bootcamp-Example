2a62f5f9e447bfbe2738aa61a8bb08a609acd9c9
yara salami <yara16salami@gmail.com>
2023-11-29 23:11:25 +0200

    Update Footer, Navbar, Mentor layout and styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import Logo from "../images/QueenB.png";
+// import Logo from "../images/QueenB.png"; // Removed the photo import
 import { Link } from "react-router-dom";
-import "./components.scss";
-
+import "./Footer.css";
 
 const Footer = () => {
-	return (
-		<footer>
-			<Link to="/">
-    			<img src={Logo} alt=""/>
-			</Link>
-		</footer>
-	);
+    const authors = ["Yara Salami", "Tanya Gendelman", "Hila Ilarov"]; // Replace with actual author names
+
+    return (
+        <footer>
+            <div className="author-list">
+                {authors.map((author, index) => (
+                    <p key={index} className="author-item">{author}</p>
+                ))}
+            </div>
+            <p className="footer-text">© {new Date().getFullYear()}</p>
+        </footer>
+    );
 };
 
 export default Footer;
