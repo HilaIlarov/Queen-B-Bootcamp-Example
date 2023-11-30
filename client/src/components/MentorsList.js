1375c1373fd73d78e10ca6768dcb1f6b7ed0b9c9
HilaIlarov <69429959+HilaIlarov@users.noreply.github.com>
2023-11-30 10:32:58 +0200

    ui changes

--- client/src/components/MentorsList.js
+++ client/src/components/MentorsList.js
@@ -42,7 +42,7 @@ const styles = {
 		height: "100%",
 		width: "70%",
 		// overflow: "auto",
-		marginLeft: "10%",
+		marginLeft: "20%",
 		marginTop: "15%",
 		marginBottom: "5%",
 		display: "flex",
