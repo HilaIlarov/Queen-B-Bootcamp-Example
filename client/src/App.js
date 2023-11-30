9b388c5fc1fbe9a2e1066e5d924d9e27cc684d44
yara salami <yara16salami@gmail.com>
2023-11-30 10:13:39 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -52,11 +52,11 @@ const router = createBrowserRouter([
 
 function App() {
 	return (
-		<div className="app">
+		//<div className="app">
 			<div className="container">
 				<RouterProvider router={router} />
 			</div>
-		</div>
+		//</div>
 	);
 }
 
--- client/src/components/MentorsList.js
+++ client/src/components/MentorsList.js
@@ -1,23 +1,13 @@
 import React from "react";
 import MentorCard from "./MentorCard";
 import firstPerson from "../images/person1.svg";
-import { Grid, Card, CardContent, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 
 
 function MentorsList(props) {
 	const { filteredMentors } = props;
 	return (
 		<div style={styles.mentorsList}>
-			{/* <Grid container spacing={2}>
-                {mentors.map((mentor, index) => (
-                    <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
-                    <MentorCard
-								mentor={mentor}
-								person={firstPerson}
-							></MentorCard>
-                    </Grid>
-                ))}
-                </Grid> */}
 			<Grid container spacing={2}>
 			{filteredMentors.map((mentor, index) => {
 				return (
@@ -27,6 +17,7 @@ function MentorsList(props) {
 					<div key={mentor.id}>
 						<MentorCard mentor={mentor} person={firstPerson}></MentorCard>
 					</div>
+
 					</Grid>
 					
 				);
@@ -41,13 +32,9 @@ const styles = {
 		flexDirection: "column",
 		height: "90%",
 		width: "90%",
-		//overflow: "auto",
 		marginTop: "3%",
 		display: "flex",
 		alignItems: "center",
-		overflowY: 'scroll',
-		// justifyContent: "center",
-		// flexwrap: "wrap",
 	},
 };
 
